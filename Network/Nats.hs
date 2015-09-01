f34dd47ac2128359c82999d7fe432fa30513dade
Stephen O'Brien <wayofthepie@gmail.com>
2015-09-01 21:21:49 +0000

    Update default msg handler

--- Network/Nats.hs
+++ Network/Nats.hs
@@ -539,7 +539,7 @@ connectionHandler' handle nats pingStatus = forever $ do
             where
                 (item, newq) = case D.popFront queue of
                     Just inq -> inq
-                    Nothing  -> ((\maybeTxt -> maybe (return ()) print maybeTxt), D.empty)
+                    Nothing  -> (maybe (return ()) print, D.empty)
         handleMessage NatsSvrPing = sendMessage nats True NatsClntPong Nothing
         handleMessage NatsSvrPong =
             atomicModifyIORef' pingStatus $
